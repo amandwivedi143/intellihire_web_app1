a07aa6930668302b941567243772752e84d178d8
amandwivedi143 <132005595+amandwivedi143@users.noreply.github.com>
2026-05-24 02:09:15 +0530

    Delete Resume-Analyser directory

--- Resume-Analyser/src/main/java/com/ai/Resume/analyser/model/Category.java
+++
@@ -1,9 +0,0 @@
-package com.ai.Resume.analyser.model;
-
-import lombok.Data;
-
-@Data
-public class Category {
-    private String tag;
-    private String label;
-}
--- Resume-Analyser/src/main/java/com/ai/Resume/analyser/model/Company.java
+++
@@ -1,8 +0,0 @@
-package com.ai.Resume.analyser.model;
-
-import lombok.Data;
-
-@Data
-public class Company {
-    private String display_name;
-}
